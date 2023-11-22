c505be9d9afba06f71748df45ba34f832f0382eb
Kirill Varchenko <kirill.varchenko@gmail.com>
2023-11-22 17:50:33 +0100

    Drop old content

--- cv-react/src/components/Page.js
+++
@@ -1,7 +0,0 @@
-import classes from "./Page.module.css";
-
-const Page = (props) => {
-  return <div className={classes.page}>{props.children}</div>;
-};
-
-export default Page;
--- cv-react/src/components/Summary.js
+++
@@ -1,7 +0,0 @@
-import classes from "./Summary.module.css";
-
-const Summary = (props) => {
-  return <p className={classes["summary"]}>{props.children}</p>;
-};
-
-export default Summary;
